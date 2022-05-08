0a085f3eafd2d56005012d58d175a0598ba5e701
tim.shen <tim.shen@farfetch.com>
2022-05-08 18:33:52 +0800

    add some key comments

--- src/main/scala/fpfinal/app/Configuration.scala
+++ src/main/scala/fpfinal/app/Configuration.scala
@@ -2,18 +2,14 @@ package fpfinal.app
 
 import cats.data._
 import fpfinal.common.IO
-import fpfinal.service.{
-  ExpenseService,
-  LiveExpenseService,
-  LivePersonService,
-  PersonService
-}
+import fpfinal.service._
 
 object Configuration {
   type IsValid[A] = Validated[NonEmptyChain[String], A]
   type Error = String
-  type ErrorOr[A] = EitherT[IO, Error, A]
-  type St[A] = StateT[ErrorOr, AppState, A]
+  type ErrorOr[A] = EitherT[IO, Error, A] // stands for IO[Either[Error, A]]
+  type St[A] = StateT[ErrorOr, AppState, A] // stands for State[AppState, ErrorOr[A]]
+//  type St1[A] = StateT[ErrorOr, AddPersonData, A] // stands for State[AppState, ErrorOr[A]]
   type SuccessMsg = String
   type Environment = ExpenseService
     with PersonService
@@ -23,6 +19,6 @@ object Configuration {
     with LivePersonService
     with LiveConsole
     with LiveController
-  type AppOp[A] = ReaderT[St, Environment, A]
+  type AppOp[A] = ReaderT[St, Environment, A] // stands for Kleisli(Environment => St[A])
   def readEnv: AppOp[Environment] = ReaderT.ask[St, Environment]
 }
